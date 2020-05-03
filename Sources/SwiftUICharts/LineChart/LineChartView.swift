aede6330b6ff4b41479d02c468098fcb2c45f82d
Andrew Che <andrewche@berkeley.edu>
2020-05-02 22:42:19 -0700

    line graph 2 data points

--- Sources/SwiftUICharts/LineChart/LineChartView.swift
+++ Sources/SwiftUICharts/LineChart/LineChartView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 public struct LineChartView: View {
     @Environment(\.colorScheme) var colorScheme: ColorScheme
     @ObservedObject var data:ChartData
+    @ObservedObject var headData:ChartData
     public var title: String
     public var legend: String?
     public var style: ChartStyle
@@ -19,6 +20,7 @@ public struct LineChartView: View {
     public var formSize:CGSize
     public var dropShadow: Bool
     public var valueSpecifier:String
+    public var frame: CGSize
     
     @State private var touchLocation:CGPoint = .zero
     @State private var showIndicatorDot: Bool = false
@@ -30,7 +32,6 @@ public struct LineChartView: View {
             
         }
     }
-    let frame = CGSize(width: 180, height: 120)
     private var rateValue: Int
     
     public init(data: [Double],
@@ -40,9 +41,11 @@ public struct LineChartView: View {
                 form: CGSize? = ChartForm.medium,
                 rateValue: Int? = 14,
                 dropShadow: Bool? = true,
-                valueSpecifier: String? = "%.1f") {
+                valueSpecifier: String? = "%.1f",
+                frame: CGSize? = CGSize(width: 360, height: 140)) {
         
         self.data = ChartData(points: data)
+        self.headData = ChartData(points: headData)
         self.title = title
         self.legend = legend
         self.style = style
@@ -51,73 +54,130 @@ public struct LineChartView: View {
         self.rateValue = rateValue!
         self.dropShadow = dropShadow!
         self.valueSpecifier = valueSpecifier!
+        self.frame = frame!
     }
     
     public var body: some View {
         ZStack(alignment: .center){
             RoundedRectangle(cornerRadius: 20)
                 .fill(self.colorScheme == .dark ? self.darkModeStyle.backgroundColor : self.style.backgroundColor)
-                .frame(width: frame.width, height: 240, alignment: .center)
+                .frame(width: frame.width, height: frame.height * 2, alignment: .center)
                 .shadow(color: self.style.dropShadowColor, radius: self.dropShadow ? 8 : 0)
-            VStack(alignment: .leading){
-                if(!self.showIndicatorDot){
-                    VStack(alignment: .leading, spacing: 8){
-                        Text(self.title)
-                            .font(.title)
-                            .bold()
-                            .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.textColor : self.style.textColor)
-                        if (self.legend != nil){
-                            Text(self.legend!)
-                                .font(.callout)
-                                .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.legendTextColor :self.style.legendTextColor)
-                        }
-                        HStack {
-                            if (self.rateValue >= 0){
-                                Image(systemName: "arrow.up")
-                            }else{
-                                Image(systemName: "arrow.down")
+            VStack(alignment: .center) {
+                // EYE DATA
+                VStack(alignment: .leading){
+                    if(!self.showIndicatorDot){
+                        VStack(alignment: .leading, spacing: 8){
+                            Text(self.title)
+                                .font(.title)
+                                .bold()
+                                .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.textColor : self.style.textColor)
+                            if (self.legend != nil){
+                                Text(self.legend!)
+                                    .font(.callout)
+                                    .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.legendTextColor :self.style.legendTextColor)
                             }
-                            Text("\(self.rateValue)%")
+//                            HStack {
+//                                if (self.rateValue >= 0){
+//                                    Image(systemName: "arrow.up")
+//                                }else{
+//                                    Image(systemName: "arrow.down")
+//                                }
+//                                Text("\(self.rateValue)%")
+//                            }
                         }
+                        .transition(.opacity)
+                        .animation(.easeIn(duration: 0.1))
+                        .padding([.leading, .top])
+                    }else{
+                        HStack{
+                            Spacer()
+                            Text("\(self.currentValue, specifier: self.valueSpecifier)")
+                                .font(.system(size: 41, weight: .bold, design: .default))
+                                .offset(x: 0, y: 30)
+                            Spacer()
+                        }
+                        .transition(.scale)
                     }
-                    .transition(.opacity)
-                    .animation(.easeIn(duration: 0.1))
-                    .padding([.leading, .top])
-                }else{
-                    HStack{
-                        Spacer()
-                        Text("\(self.currentValue, specifier: self.valueSpecifier)")
-                            .font(.system(size: 41, weight: .bold, design: .default))
-                            .offset(x: 0, y: 30)
-                        Spacer()
+                    Spacer()
+                    GeometryReader{ geometry in
+                        Line(data: self.data,
+                            frame: .constant(geometry.frame(in: .local)),
+                            touchLocation: self.$touchLocation,
+                            showIndicator: self.$showIndicatorDot,
+                            minDataValue: .constant(nil),
+                            maxDataValue: .constant(nil)
+                        )
                     }
-                    .transition(.scale)
-                }
-                Spacer()
-                GeometryReader{ geometry in
-                    Line(data: self.data,
-                        frame: .constant(geometry.frame(in: .local)),
-                        touchLocation: self.$touchLocation,
-                        showIndicator: self.$showIndicatorDot,
-                        minDataValue: .constant(nil),
-                        maxDataValue: .constant(nil)
-                    )
-                }
-                .frame(width: frame.width, height: frame.height + 30)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                .offset(x: 0, y: 0)
-            }.frame(width: self.formSize.width, height: self.formSize.height)
+                    .frame(width: frame.width, height: frame.height + 30)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    .offset(x: 0, y: 0)
+                }.frame(width: self.frame.width, height: self.frame.height)
+                .gesture(DragGesture()
+                .onChanged({ value in
+                    self.touchLocation = value.location
+                    self.showIndicatorDot = true
+                    self.getClosestDataPoint(toPoint: value.location, width:self.frame.width, height: self.frame.height)
+                })
+                    .onEnded({ value in
+                        self.showIndicatorDot = false
+                    })
+                )
+                
+                // HEAD DATA
+                VStack(alignment: .leading){
+                    if(!self.showIndicatorDot){
+//                        VStack(alignment: .leading, spacing: 8){
+//                            Text(self.title)
+//                                .font(.title)
+//                                .bold()
+//                                .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.textColor : self.style.textColor)
+//                            if (self.legend != nil){
+//                                Text(self.legend!)
+//                                    .font(.callout)
+//                                    .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.legendTextColor :self.style.legendTextColor)
+//                            }
+//                        }
+//                        .transition(.opacity)
+//                        .animation(.easeIn(duration: 0.1))
+//                        .padding([.leading, .top])
+                    }else{
+                        HStack{
+                            Spacer()
+                            Text("\(self.currentValue, specifier: self.valueSpecifier)")
+                                .font(.system(size: 41, weight: .bold, design: .default))
+                                .offset(x: 0, y: 30)
+                            Spacer()
+                        }
+                        .transition(.scale)
+                    }
+                    Spacer()
+                    GeometryReader{ geometry in
+                        Line(data: self.headData,
+                            frame: .constant(geometry.frame(in: .local)),
+                            touchLocation: self.$touchLocation,
+                            showIndicator: self.$showIndicatorDot,
+                            minDataValue: .constant(nil),
+                            maxDataValue: .constant(nil)
+                        )
+                    }
+                    .frame(width: frame.width, height: frame.height + 30)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    .offset(x: 0, y: 0)
+                }.frame(width: self.frame.width, height: self.frame.height)
+                .gesture(DragGesture()
+                .onChanged({ value in
+                    self.touchLocation = value.location
+                    self.showIndicatorDot = true
+                    self.getClosestDataPoint(toPoint: value.location, width:self.frame.width, height: self.frame.height)
+                })
+                    .onEnded({ value in
+                        self.showIndicatorDot = false
+                    })
+                )
+            }
         }
-        .gesture(DragGesture()
-        .onChanged({ value in
-            self.touchLocation = value.location
-            self.showIndicatorDot = true
-            self.getClosestDataPoint(toPoint: value.location, width:self.frame.width, height: self.frame.height)
-        })
-            .onEnded({ value in
-                self.showIndicatorDot = false
-            })
-        )
+        
     }
     
     @discardableResult func getClosestDataPoint(toPoint: CGPoint, width:CGFloat, height: CGFloat) -> CGPoint {
